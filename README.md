215b84f1d55afd6978e1329e07f94f69253514f4
Mark Mercado <mamercad@umflint.edu>
2016-08-03 14:23:20 -0400

    small enhancements

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # Slack to Zabbix
 
-Zabbix alertscript that sends triggers to Slack.
+Zabbix alertscript that sends triggers to Slack. There are likely numerous of these in the wild, but this one is mine, and I prefer to use my own stuff sometimes.
 
 ## Installation
 
 1. All you really need from this repo is `slack.py`, the image `zabbix.png` is called from the script over HTTP from this Github repo. Copy `slack.py` into your Zabbix alertscripts directory, typically something like `/usr/lib/zabbix/alertscripts`.
 
-2. In the Zabbix interface, create a new "Media Type" called `Slack` located in the Administration section:
-  * Name: Slack
-  * Type: Script
-  * Script name: slack.py
+2. In the Zabbix interface, create a new `Media Type` called `Slack` located in the Administration section:
+  * Name: `Slack`
+  * Type: `Script`
+  * Script name: `slack.py`
   * Script parameters:
-    * {ALERT.SENDTO}
-    * {ALERT.SUBJECT}
-    * {ALERT.MESSAGE}
-  * Enabled: checked
+    * `{ALERT.SENDTO}`
+    * `{ALERT.SUBJECT}`
+    * `{ALERT.MESSAGE}`
+  * Enabled: `checked`
 
 3. Create a user named `slack` also in the Administration section:
-  * Alias: slack
-  * Name: Slack
-  * Last Name: Notify
-  * Groups: Read only
+  * Alias: `slack`
+  * Name: `Slack`
+  * Last Name: `Notify`
+  * Groups: `Read only`
   * Password: (up to you)
   * Language: (up to you)
   * Theme: (up to you)
@@ -31,10 +31,11 @@ Zabbix alertscript that sends triggers to Slack.
   * URL (after login): doesn't matter
 
 4. Under the previous user's `Media` add `Slack`:
-  * Type: Slack
-  * Send to: SLACK_API_KEY,#SLACK_CHANNEL
+  * Type: `Slack`
+  * Send to: `SLACK_API_KEY,#SLACK_CHANNEL`
+    * For example: `xoxp-?-?-?-?,#general`
   * When active: (up to you)
   * Use if severity: (up to you)
-  * Enabled: checked
+  * Enabled: `checked`
 
 That should be it.
--- slack.py
+++ slack.py
@@ -6,82 +6,137 @@ import json
 import re
 import requests
 import sys
+import time
 
 try:
 
-  # slack api endpoint
-  SLACK_API = 'https://slack.com/api/chat.postMessage'
-  SLACK_USERNAME = 'Zabbix'
-  SLACK_ICON = 'https://github.com/mamercad/zabbix-to-slack/raw/master/zabbix.png'
+    # what time is it?
+    EPOCH = int(time.time())
 
-  # zabbix color lookup
-  COLOR = {
-    'OK': '#00AA00',
-    'PROBLEM': {
-      'Not classified': '#97AAB3',
-      'Information': '#7499FF',
-      'Warning': '#FFC859',
-      'Average': '#FFA059',
-      'High': '#E97659',
-      'Disaster': '#E45959'
+    # github repo (attribution)
+    GIT_REPO = 'https://github.com/mamercad/zabbix-to-slack'
+
+    # logging knob
+    LOGGING = True
+
+    # slack api endpoint
+    SLACK_API = 'https://slack.com/api/chat.postMessage'
+    SLACK_USERNAME = 'Zabbix'
+    SLACK_ICON = 'https://github.com/mamercad/zabbix-to-slack/raw/master/zabbix.png'
+
+    # zabbix color lookup
+    COLOR = {
+        'OK': '#00AA00',
+        'PROBLEM': {
+            'Not classified': '#97AAB3',
+            'Information': '#7499FF',
+            'Warning': '#FFC859',
+            'Average': '#FFA059',
+            'High': '#E97659',
+            'Disaster': '#E45959'
+        }
     }
-  }
 
-  # enable logging
-  logging.basicConfig(filename='/tmp/slack.log', level=logging.DEBUG)
+    # enable logging
+    if LOGGING:
+        logging.basicConfig(filename='/tmp/slack.log', level=logging.DEBUG)
 
-  # parse the command line args
-  AUTHTOKEN_COMMA_CHANNEL = sys.argv[1]
-  SUBJECT_NAME = sys.argv[2]
-  MESSAGE_FULL = sys.argv[3]
+    # parse the command line args
+    AUTHTOKEN_COMMA_CHANNEL = sys.argv[1]
+    SUBJECT_NAME = sys.argv[2]
+    MESSAGE_FULL = sys.argv[3]
 
-  # logging start
-  logging.debug('{} {} {}'.format('='*20, str(datetime.datetime.now()), '='*20))
-  logging.debug(sys.argv)
+    # logging start
+    if LOGGING:
+        logging.debug('{} {} {}'.format('=' * 20, str(datetime.datetime.now()), '=' * 20))
+        logging.debug(sys.argv)
 
-  # parse the args further
-  (AUTH_TOKEN, SLACK_CHANNEL) = AUTHTOKEN_COMMA_CHANNEL.split(',')
-  (SUBJECT, NAME) = SUBJECT_NAME.split(': ')
-  (TRIGGER, STATUS, SEVERITY, URL, VALUE, EVENT) = MESSAGE_FULL.split('____')
+    # parse the args further
+    (AUTH_TOKEN, SLACK_CHANNEL) = AUTHTOKEN_COMMA_CHANNEL.split(',')
+    (SUBJECT, NAME) = SUBJECT_NAME.split(': ')
+    (TRIGGER, STATUS, SEVERITY, URL, VALUE, EVENT) = MESSAGE_FULL.split('____')
 
-  # logging.debug(TRIGGER + ',' + STATUS + ',' + SEVERITY + ',' + URL + ',' + VALUE + ',' + EVENT)
+    if LOGGING:
+        logging.debug(TRIGGER + ',' + STATUS + ',' + SEVERITY +
+                      ',' + URL + ',' + VALUE + ',' + EVENT)
 
-  DISPLAY_COLOR = ''
-  if SUBJECT == 'PROBLEM':
-    DISPLAY_COLOR = COLOR['PROBLEM'][SEVERITY]
-  else:
-    DISPLAY_COLOR = COLOR['OK']
+    # lookup the display color
+    DISPLAY_COLOR = ''
+    if SUBJECT == 'PROBLEM':
+        DISPLAY_COLOR = COLOR['PROBLEM'][SEVERITY]
+    else:
+        DISPLAY_COLOR = COLOR['OK']
 
-  # prepare slack payload
-  payload = {
-      'token': AUTH_TOKEN,
-      'channel': SLACK_CHANNEL,
-      'username': SLACK_USERNAME,
-      'as_user': 'false',
-      'icon_url': SLACK_ICON,
-      'text': SUBJECT_NAME,
-      'attachments': json.dumps([
-          {
-              'fields': [
-                  {'title': 'Trigger', 'value': TRIGGER, 'short': 'true'},
-                  {'title': 'Trigger status', 'value': STATUS, 'short': 'true'},
-                  {'title': 'Trigger severity', 'value': SEVERITY, 'short': 'true'},
-                  {'title': 'Trigger URL', 'value': URL, 'short': 'true'},
-                  {'title': 'Item value', 'value': VALUE, 'short': 'true'},
-                  {'title': 'Original event ID', 'value': EVENT, 'short': 'true'}
-              ],
-              'color': DISPLAY_COLOR
-          }
-      ])
-  }
+    # prepare slack payload
+    payload = {
+        'token': AUTH_TOKEN,
+        'channel': SLACK_CHANNEL,
+        'username': SLACK_USERNAME,
+        'as_user': 'false',
+        'icon_url': SLACK_ICON,
+        'title': '*' + SUBJECT_NAME + '*',
+        'text': '*' + SUBJECT_NAME + '*',
+        'attachments': json.dumps([
+            {
+                'color': DISPLAY_COLOR,
+                'fallback': TRIGGER + ' ' + STATUS + ' ' + SEVERITY,
+                'fields': [
+                    {
+                        'title': 'Trigger', 'value': TRIGGER,
+                        'short': 'true'
+                    },
+                    {
+                        'title': 'Trigger status', 'value': STATUS,
+                        'short': 'true'
+                    },
+                    {
+                        'title': 'Trigger severity', 'value': SEVERITY,
+                        'short': 'true'
+                    },
+                    {
+                        'title': 'Trigger URL', 'value': URL,
+                        'short': 'true'
+                    },
+                    {
+                        'title': 'Item value', 'value': VALUE,
+                        'short': 'true'
+                    },
+                    {
+                        'title': 'Original event ID', 'value': EVENT,
+                        'short': 'true'
+                    }
+                ],
+                'footer': GIT_REPO,
+                'pretext': 'Zabbix trigger information',
+                'ts': EPOCH
+            },
+            # {
+            #     "attachment_type": "default",
+            #     "color": "#3AA3E3",
+            #     "fallback": "Zabbix trigger ack",
+            #     "callback_id": "zabbix_trigger_ack",
+            #     "actions": [
+            #         {
+            #             "name": "ack",
+            #             "text": "Acknowledge",
+            #             "type": "button",
+            #             "value": "ack"
+            #         }
+            #     ]
+            # }
+        ]),
+    }
 
-  # logging.debug(json.dumps(payload))
+    if LOGGING:
+        logging.debug(json.dumps(payload))
 
-  # post the alert to slack
-  r = requests.post(SLACK_API, data=payload)
-  logging.debug(r.status_code)
-  logging.debug(r.json())
+    # post the alert to slack
+    r = requests.post(SLACK_API, data=payload)
+
+    if LOGGING:
+        logging.debug(r.status_code)
+        logging.debug(r.json())
 
 except Exception as e:
 
-  logging.debug(e, exc_info=True)
+    logging.debug(e, exc_info=True)
